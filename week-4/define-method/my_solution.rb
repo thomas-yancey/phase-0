3295204f5b06036530da7384af1b3369f5bff3b0
thomas-yancey <tomyncy618@gmail.com>
2015-12-24 17:25:03 -0500

    Update my_solution.rb

--- week-4/define-method/my_solution.rb
+++ week-4/define-method/my_solution.rb
@@ -1,5 +1,5 @@
 # Define an Empty Method
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 # Your Solution Below
 
 def do_something(param1, param2, param3)
